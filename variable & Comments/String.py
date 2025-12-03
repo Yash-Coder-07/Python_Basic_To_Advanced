85070da38ef813eaf9b4d2c300ed33d014e3076d
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-03 17:55:17 +0530

    Variable

---
+++ variable & Comments/String.py
@@ -0,0 +1,14 @@
+# 
+#a = "A"
+#print(ord(a)) # converts into unique code 
+
+#a = 65
+#print(chr(a)) # converts the unique code into characters
+
+#a  = "Hello World"
+#print(a[-2])
+
+# String Slicing
+#b = "SHER CODER"
+
+#print(b[0:4:1]) # [start : end(not include) : step] if not mentioed any then its default
---
+++ variable & Comments/TypeConversion.py
@@ -0,0 +1,19 @@
+a = 12
+# conversion of int to string
+a =str(a)
+print(type(a))
+
+b = "13"
+# in string there is charcter then not possible to convert to int
+b = int(a)
+print(type(b))
+
+c = 12
+print(bool(c))
+
+""" There ar some falsy values which always convert into fals  and remaining in true value
+false values are --> false, 0 , 0.0, " ",[list],(tuples),{dictionary} """
+
+## Implicit type conversion 
+# automatically converts the data types on its own
+""" when 12/3 output is not 4 its 4.0 which is float """ 
