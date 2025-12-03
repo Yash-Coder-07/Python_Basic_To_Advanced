deead4da756ef67ea0e425b6d73ae21eafaba820
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-03 18:05:37 +0530

    InputOutput

---
+++ Input & Output/input.py
@@ -0,0 +1,16 @@
+name = "Yash"
+age = "20"
+
+#print(name,age)
+
+#print("Hello my name is :",name,"my age is:",age)
+
+# Below is formatted string
+#No need to add more string always
+#print(f"My name is {name} : My age is {age}")
+
+
+### Input statment
+
+man = input("Hello what is your age")
+print(age)
