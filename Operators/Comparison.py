014c7a9412c6ca4c2cadca19d37089b4895fa4b0
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-06 14:42:21 +0530

    Logical

---
+++ Operators/Comparison.py
@@ -0,0 +1,14 @@
+a = 12
+b = 12
+#print(a==b)
+#print(a!=b)
+
+# in python we can cokpare int and float
+
+#print(23>23)
+
+print("A" > "B")
+# check o precedance 
+print("ABC" > "ACD")
+
+## We cannot check the different data types
