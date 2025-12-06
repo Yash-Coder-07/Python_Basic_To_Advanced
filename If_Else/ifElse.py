3265417722b0aa45a9c6721a919a0a48f7d0b8d9
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-06 16:06:04 +0530

    If_Else

---
+++ If_Else/ifElse.py
@@ -0,0 +1,28 @@
+a = 7
+
+# after write if there i sspace and then condition
+#if a > 10:
+#    print("I will do task A")
+#else:
+#    print("I will do task B") 
+
+#money = int(input("Please provide the money: "))
+
+#if money>80:
+#    print("I will bring magnun ")
+
+#elif money>20 and money <80:
+#    print("I will bring Coornato")
+#else:
+#    print("I will bring Choco-Bar ")
+
+"""  Practice  Question """
+
+#gen = input("Tell me your gender (M or F): ")
+
+#if gen =="M" or gen =="m":
+#    print("Good Morning Sir")
+#elif gen =="F" or gen=="f":
+#    print("Good Morning Mam")
+#else:
+#    print("Provide the proper gender M or F ")
