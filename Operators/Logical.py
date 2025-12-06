7e03cd88f506c29c4221794a49138b02c50800e2
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-06 15:01:36 +0530

    Logical

---
+++ Operators/Logical.py
@@ -0,0 +1,21 @@
+#  and Operator
+
+#print(123 > 100 and 34==34)
+#print("a" < "c" and 234==234 and 34==34 and 90<45)
+
+# or operator
+
+#print( 12 != 12 or 23==45 or 67==65 or 10 >5)
+
+# not Operator
+# not operator revrse the ouput 
+# If it is true then convert it into false 
+# if it is false converts it into true 
+
+#print(not 12==12)
+
+# True != False...Give True 
+print((456==456) != (235==236)) 
+
+# it gives false bcz bool(0) is False
+print(True and bool(0))
