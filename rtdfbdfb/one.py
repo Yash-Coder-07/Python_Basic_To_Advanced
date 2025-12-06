852e1d9077a752ab433908d01ade97ec7a4343c9
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-06 13:06:55 +0530

    Restart

---
+++ rtdfbdfb/one.py
@@ -0,0 +1,29 @@
+# Cannot add number starting of varible name
+# cannot use space in variable name
+# Not ue special character in variable
+
+#SheriyansSchool = "students" # Pascal acse
+
+#sheriyansSchool = "students" # camel case
+
+#sheriyan_school = "students" # snake case
+
+"""  Integer
+a = 12
+print(type(a))
+
+b = 1.5
+print(type(b))
+
+c = 12/3   
+print(type(c))  # still tgis is float
+
+v = 34j
+print(type(v)) # By adding j after value make it complex number"""
+
+
+# everything between inverted comma it is tsring
+
+s = 'e3r423f0'
+print(type(s))
+
