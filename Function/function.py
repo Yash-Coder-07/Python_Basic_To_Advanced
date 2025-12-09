a0b20099893d06bc53cea1bbf6924a209fa275f8
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-09 19:51:03 +0530

    Function Part 2

--- Function/function.py
+++ Function/function.py
@@ -8,3 +8,14 @@
 #     print(f"Sum of your numbers is: {c}")
 # sum(5,6)
 
+
+def hello(name,age):
+    print(f"Your name is {name} and your age is {age}")
+
+# Positional Argument Sequence of argument must be same
+hello("Yash","20") 
+
+# Keyword Argument sequence of argument doesnt matter
+hello(age = 21, name ="Pranav") 
+
+# Deafult Argument
