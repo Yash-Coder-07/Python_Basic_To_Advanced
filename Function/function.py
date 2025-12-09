e58d917f2daf378b8db9c88f5b0f51ed89cdf746
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-09 23:10:06 +0530

    Function Complete

--- Function/function.py
+++ Function/function.py
@@ -9,13 +9,34 @@
 # sum(5,6)
 
 
-def hello(name,age):
-    print(f"Your name is {name} and your age is {age}")
+# def hello(name,age):
+#     print(f"Your name is {name} and your age is {age}")
 
 # Positional Argument Sequence of argument must be same
-hello("Yash","20") 
+#hello("Yash","20") 
 
 # Keyword Argument sequence of argument doesnt matter
-hello(age = 21, name ="Pranav") 
+#hello(age = 21, name ="Pranav") 
 
-# Deafult Argument
+# Default Argument
+# def sum(a,b=45):
+#     print(f"The sum is {a+b}")
+
+# sum(12) # No need to pass the b value 
+# sum(12,34) # b value is override 
+ 
+# String is palindrome or not
+
+def palindrome(str):
+    rev =""
+    for i in range(len(str)-1,-1,-1):
+        rev = rev + str[i]
+    
+    if rev==str:
+        print("Palindrome")
+    else:
+        print("Not Palindrome") 
+
+palindrome("m adam")
+palindrome("Yash")
+palindrome("qwerrewq")
