df8032c8bf1aa099507334b7a5cefd1b10ded8ac
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-11 10:56:32 +0530

    Tuple

---
+++ Tuples/tuple.py
@@ -0,0 +1,23 @@
+# a =(1,2,3,4)
+# print(type(a)) 
+
+# b =(1,2,3,5,5,6.7,89,print(),"Hello",True)
+# print(b)
+
+# index = b.index(6.7)
+# print(index)
+
+# count = b.count(5)
+# print(count)
+
+
+# Tuple Unpacking
+# a captrure 1, b capture 2, c cpature 3,d capture 4
+# it has type of int of a,b,c,d
+# a,b,c,d=(1,2,3,4)
+# print(b)
+# print(type(b))
+
+#Now it has type as Tuple
+# a =(1,)
+# print(type(a))
