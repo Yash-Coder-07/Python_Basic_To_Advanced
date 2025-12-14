172bad3c377e3d4e42287af85f2c4d683015bf57
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-12-14 15:10:39 +0530

    OOP Object Class

--- OOPs/main.py
+++ OOPs/main.py
@@ -1,12 +1,21 @@
 class Factory:
     a = 12   # Attribute
     
-    def hello(self):
+    def hello(self):     # Method
         print("How are you")
     
     print("Hello how are you I am getting initialized")
 
-Factory()
+# Factory()
 
-Factory().hello() #accessing methods in class
-print(Factory().a)  #accessing attribute in class
+# Factory().hello() #accessing methods in class
+# print(Factory().a)  #accessing attribute in class
+
+
+""" ..........What is Object......r....."""
+# here obj is now Object it has all power related to class
+# When Python defines the class Factory, it executes everything 
+# inside the class body immediately, except method bodies.
+obj = Factory()
+print(obj.a)
+obj.hello()
